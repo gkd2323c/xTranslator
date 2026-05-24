c0c3ea04a1d80705381e98bbc9de4575230c7705
gkd2323c <216696112+gkd2323c@users.noreply.github.com>
2026-05-24 19:06:16 +0800

    chore: remove accidentally committed tool artifacts

--- codegraph-mcp.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\gkd2323c\Documents\xTranslator"
-set CODEGRAPH_FORCE_WATCH=1
-"node" "C:\Users\gkd2323c\AppData\Roaming\npm\node_modules\@colbymchenry\codegraph\dist\bin\codegraph.js" serve --mcp
